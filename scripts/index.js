c8dda2c38b1dbc492a22dc6584d9679b5812fff6
Evgeny Shigaev <shigaev-evgeniy@yandex.ru>
2022-12-07 23:13:26 +0400

    Проектная работа №4. Итерация 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,18 @@
-// проверка подключен ли скрипт
-console.log("check");
-
 // подключение dom-элементов
-const popupElement = document.querySelector(".popup");
-const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
-const popupOpenButtonElement = document.querySelector(".profile__edit-button");
-const popupFormElement = popupElement.querySelector(".popup__form");
-const nameInput = popupElement.querySelector(".popup__input_data_name");
-const jobInput = popupElement.querySelector(".popup__input_data_job");
-const profileName = document.querySelector(".profile__name");
-const profileJob = document.querySelector(".profile__job");
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupFormElement = popupElement.querySelector('.popup__form');
+const nameInput = popupElement.querySelector('.popup__input_data_name');
+const jobInput = popupElement.querySelector('.popup__input_data_job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
 
 // открытие попапа
 const openPopup = function() {
-  popupElement.classList.toggle("popup__opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+  popupElement.classList.toggle('popup_opened');
 }
 
 // сохранение информации из инпутов
@@ -29,21 +26,10 @@ function formSubmitHandler(evt) {
 
 // закрытие попапа
 const closePopup = function() {
-  popupElement.classList.toggle("popup__opened");
-}
-
-// закрытие попапа при нажатии на область вне него
-const closePopupByClickOnOverlay = function(event) {
-  console.log(event.target, event.currentTarget);
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-
-  closePopup();
+  popupElement.classList.toggle('popup_opened');
 }
 
 // регистрация обработчиков по клику
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
 popupFormElement.addEventListener('submit', formSubmitHandler);
